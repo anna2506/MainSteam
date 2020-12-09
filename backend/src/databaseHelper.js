551f488e6292e25a9cd27f11e4277ab06bc83a3d
Anna Doshchenko <annadoshenko3@gmail.com>
2020-12-09 22:54:24 +0300

    Added requests on backend to database

--- backend/src/databaseHelper.js
+++ backend/src/databaseHelper.js
@@ -8,25 +8,105 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getUserByLogin = async (login) => {
-  const users = await pool.query('SELECT * FROM player WHERE login = $1', [login]);
-  return users.rows && users.rows[0];
+const getPlayerByLogin = async (login) => {
+  const player = await pool.query('SELECT * FROM player WHERE login = $1', [login]);
+  return player.rows[0];
 };
 
-const getUserByEmail = async (email) => {
-  const users = await pool.query('SELECT * FROM player WHERE email = $1', [email]);
-  return users.rows && users.rows[0];
+const getPlayerByEmail = async (email) => {
+  const player = await pool.query('SELECT * FROM player WHERE email = $1', [email]);
+  return player.rows[0];
 };
 
-const saveUser = async (login, email, hash, country, token) => {
+const savePlayer = async (login, email, hash, country, token) => {
   await pool.query(`
     INSERT INTO player (login, email, pass_hash, country, token)
     VALUES ($1, $2, $3, $4, $5)
   `, [login, email, hash, country, token]);
 };
 
+const getPlayerInfo = async (token) => {
+  const player = await pool.query(`
+    SELECT
+      login, experience, country, email
+    FROM player
+    WHERE token = $1`,
+  [token]);
+  return player.rows[0];
+};
+
+const getGame = async (id) => {
+  const game = await pool.query('SELECT * FROM game WHERE id = $1', [id]);
+  return game.rows[0];
+};
+
+const getGames = async () => {
+  const games = await pool.query('SELECT * FROM game');
+  return games.rows;
+};
+
+const getPlayerGames = async (playerId) => {
+  const playerGames = await pool.query(
+    'SELECT * FROM player_game WHERE player_id = $1',
+    [playerId],
+  );
+  return playerGames.rows[0];
+};
+
+const updatePlayer = async (login, experience, country, email, id) => {
+  await pool.query(`
+  UPDATE player SET login = $1, experience = $2, country = $3, email = $4 WHERE id = $4
+  `, [login, experience, country, email, id]);
+};
+
+const savePlayerGame = async (
+  playerId,
+  gameId,
+  timeSpent,
+  highScore,
+  beat100,
+  beat200,
+  beat400,
+  beat700,
+  beat1000,
+  beat10000,
+) => {
+  await pool.query(`
+    INSERT INTO player_game
+    (
+      player_id, game_id, time_spent, high_score, beat_100,
+      beat_200, beat_400, beat_700, beat_1000, beat_10000
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    ON CONFLICT (player_id, game_id)
+    DO UPDATE player_game
+    SET 
+      timeSpent = $3, highScore = $4, beat100 = $5,
+      beat200 = $6, beat400 = $7, beat700 = $8,
+      beat1000 = $9, beat10000 = $10 WHERE player_id = $11
+  `,
+  [
+    playerId,
+    gameId,
+    timeSpent,
+    highScore,
+    beat100,
+    beat200,
+    beat400,
+    beat700,
+    beat1000,
+    beat10000,
+  ]);
+};
+
 module.exports = {
-  getUserByLogin,
-  getUserByEmail,
-  saveUser,
+  getPlayerByLogin,
+  getPlayerByEmail,
+  savePlayer,
+  getPlayerInfo,
+  getGame,
+  getGames,
+  getPlayerGames,
+  savePlayerGame,
+  updatePlayer,
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,15 @@ const errorMessages = require('./errorMessages');
 const authHelper = require('./authHelper');
 const tokenHelper = require('./token');
 
+const getPlayerInfo = async (header) => {
+  const { token } = header;
+  const player = await dbHelper.getPlayerInfo(token);
+  if (!player) {
+    return { error: errorMessages.accessDenied };
+  }
+  return player;
+};
+
 module.exports = (app) => {
   app.get('/', (request, response) => response.json({ info: 'Welcome to the server' }));
 
@@ -15,15 +24,15 @@ module.exports = (app) => {
       email,
       country,
     } = request.body;
-    if (await dbHelper.getUserByLogin(login)) {
+    if (await dbHelper.getPlayerByLogin(login)) {
       return response.status(500).json({ message: errorMessages.loginDuplicate });
     }
-    if (await dbHelper.getUserByEmail(email)) {
+    if (await dbHelper.getPlayerByEmail(email)) {
       return response.status(500).json({ message: errorMessages.emailDuplicate });
     }
     const hash = await authHelper.generatePasswordHash(password);
     const token = tokenHelper.generateToken(login, email);
-    await dbHelper.saveUser(login, email, hash, country, token);
+    await dbHelper.savePlayer(login, email, hash, country, token);
     return response.json({ token });
   });
 
@@ -32,13 +41,119 @@ module.exports = (app) => {
       password,
       login,
     } = request.body;
-    const user = await dbHelper.getUserByLogin(login);
-    if (!user) {
+    const player = await dbHelper.getPlayerByLogin(login);
+    if (!player) {
       return response.status(500).json({ message: errorMessages.wrongLogin });
     }
-    if (await authHelper.checkPassword(password, user.pass_hash)) {
-      return response.json({ token: user.token });
+    if (await authHelper.checkPassword(password, player.pass_hash)) {
+      return response.json({ token: player.token });
     }
     return response.status(500).json({ message: errorMessages.wrongPassword });
+  });
+
+  app.get('/player', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(404).json({ message: errorMessages.playerNotFound });
+    }
+    return response.json({ ...player });
+  });
+
+  app.get('/games/{id}', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(500).json({ message: player.error });
+    }
+    const { id } = request.params;
+    const game = await dbHelper.getGame(id);
+    if (!game) {
+      return response.status(404).json({ message: errorMessages.gameNotFound });
+    }
+    return response.json({ ...game });
+  });
+
+  app.get('/games', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(500).json({ message: player.error });
+    }
+    const games = await dbHelper.getGames();
+    return response.json([...games]);
+  });
+
+  app.get('/playerGame', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(500).json({ message: player.error });
+    }
+    const playerGames = await dbHelper.getPlayerGames(player.id);
+    playerGames.map((playerGame) => ({
+      gameId: playerGame.game_id,
+      timeSpent: playerGame.time_spent,
+      highScore: playerGame.high_score,
+      beat100: playerGame.beat_100,
+      beat200: playerGame.beat_200,
+      beat400: playerGame.beat_400,
+      beat700: playerGame.beat_700,
+      beat1000: playerGame.beat_1000,
+      beat10000: playerGame.beat_10000,
+    }));
+    return response.json([...playerGames]);
+  });
+
+  app.post('/playerGame', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(500).json({ message: player.error });
+    }
+    const {
+      playerId,
+      gameId,
+      timeSpent,
+      highScore,
+      beat100,
+      beat200,
+      beat400,
+      beat700,
+      beat1000,
+      beat10000,
+    } = request.body;
+    try {
+      await dbHelper.savePlayerGame(
+        playerId,
+        gameId,
+        timeSpent,
+        highScore,
+        beat100,
+        beat200,
+        beat400,
+        beat700,
+        beat1000,
+        beat10000,
+      );
+      return response.status(200);
+    } catch (e) {
+      return response.status(500).json({ message: e.message });
+    }
+  });
+
+  app.put('/player', async (request, response) => {
+    const player = await getPlayerInfo(request.header);
+    if (player.error) {
+      return response.status(500).json({ message: player.error });
+    }
+    const {
+      id,
+      login,
+      experience,
+      country,
+      email,
+    } = request.body;
+    try {
+      await dbHelper.updatePlayer(login, experience, country, email, id);
+      return response.status(200);
+    } catch (e) {
+      return response.status(500).json({ message: e.message });
+    }
   });
 };
